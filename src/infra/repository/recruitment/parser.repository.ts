a1d2268d3717836874c01ece7168901e82e0eafc
em-le <emltk@zigexn.vn>
2024-12-23 21:45:20 +0700

    fix: repo errors

--- src/infra/repository/recruitment/parser.repository.ts
+++ src/infra/repository/recruitment/parser.repository.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { MongooseRepositoryAbstract } from 'src/internal/database/abstracts/repository.abstract';
-import { ParserDocument, Parser } from './schemas/Parser.schema';
+import { ParserDocument, Parser } from './schemas/parser.schema';
 import { MongooseModel } from 'src/internal/database/decorators/database.decorator';
 
 @Injectable()
 export class ParserRepository extends MongooseRepositoryAbstract<ParserDocument> {
   constructor(
     @MongooseModel(Parser.name)
-    private readonly ParserModel: Model<ParserDocument>,
+    private readonly parserModel: Model<ParserDocument>,
   ) {
-    super(ParserModel);
+    super(parserModel);
   }
 
   //
