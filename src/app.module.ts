7214513cfa00fc437cbdea425052735d71b6cdee
em-le <emltk@zigexn.vn>
2024-12-31 12:48:47 +0700

    fea: static server

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,18 @@ import { InternalModule } from './internal/internal.module';
 import { RouterModule } from './router/router.module';
 import { SchedulerModule } from './scheduler/scheduler.module';
 import { QueueModule } from './queue/queue.module';
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 @Module({
   imports: [
     InternalModule,
     RouterModule.register(),
     SchedulerModule.forRoot(),
     QueueModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'storage'),
+      serveRoot: '/storage',
+    }),
   ],
   controllers: [],
   providers: [],
